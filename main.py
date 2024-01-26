05ab5da049a1b79205ee0b7c7d3657a7d5b8195c
MaksymBratsiun <bratsyun_m@ukr.net>
2024-01-26 19:30:27 +0200

    add links scrap main.py todo vin, num, phone, count

--- main.py
+++ main.py
@@ -14,20 +14,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
-
-# URL = "https://auto.ria.com/uk/auto_bmw_x5_35920151.html"
-URL = "https://auto.ria.com/uk/auto_bmw_x6_35935575.html"
+URL_USED = "https://auto.ria.com/uk/car/used/"
+URL = "https://auto.ria.com/uk/auto_bmw_x5_35920151.html"
+# URL = "https://auto.ria.com/uk/auto_bmw_x6_35935575.html"
 
 load_dotenv()
 
-# username = os.environ.get("LOGIN_USERNAME")
-# password = os.environ.get("LOGIN_PASSWORD")
-#
-# login_url = 'https://auto.ria.com/uk/login'
-# login_payload = {
-#     'username': username,
-#     'password': password
-#     }
+username = os.environ.get("LOGIN_USERNAME")
+password = os.environ.get("LOGIN_PASSWORD")
+
+login_url = 'https://auto.ria.com/uk/login'
+login_payload = {
+    'username': username,
+    'password': password
+    }
 
 
 def scrap_data(target_url):
@@ -43,9 +43,11 @@ def scrap_data(target_url):
     options.add_argument('--headless=chrome')
 
     with webdriver.Chrome(service=service, options=options) as driver:
-        # image_url = scrap_image(driver, URL)
-        image_url = None
-        phone_number = scrap_phone(driver, URL)
+        image_url = scrap_image(driver, target_url)
+        phone_number = scrap_phone(driver, target_url)
+        car_number = scrap_car_number(driver, target_url)
+        car_vin = scrap_vin(driver, target_url)
+
 
     response = requests.get(target_url)
     soup = BeautifulSoup(response.text, 'lxml')
@@ -54,11 +56,10 @@ def scrap_data(target_url):
     price_usd = processed_price(soup.find('div', class_='price_value').text)
     odometer = processed_odometer(soup.find('div', class_='base-information bold').text)
     username_ = soup.find('div', class_='seller_info_name bold').text.strip()
-
-    images_count = processed_img_count(soup.find('span', class_='count').text)
-    car_number = soup.find('span', class_='state-num ua')  # need selenium
-    car_vin = soup.find('span', class_='vin-code').text  # need selenium
-    car_vin_1 = soup.find('span', class_='label-vin')  # need selenium
+    images_count = processed_img_count(soup.find('div', class_='action_disp_all_block').text)
+    # images_count = processed_img_count(soup.find('span', class_='count').text)
+    # car_vin = soup.find('span', class_='vin-code').text  # need selenium
+    # car_vin_1 = soup.find('span', class_='label-vin')  # need selenium
     datetime_found = datetime.date.today()
 
     return {
@@ -86,9 +87,10 @@ def processed_odometer(input_odometer):
     return int(res)*1000
 
 
+#TODO
 def processed_img_count(input_count):
-    res = input_count.split('з')[1].replace(' ', '')
-    return int(res)
+    count_img = input_count.strip().split(' ')
+    return int(count_img[2])
 
 
 def scrap_image(driver, target_url):
@@ -100,30 +102,34 @@ def scrap_image(driver, target_url):
     return src_value
 
 
+#TODO
 def scrap_phone(driver, target_url):
     driver.get(target_url)
-    div_element = driver.find_element(By.XPATH, '/html//div[@class="photo-620x465 loaded"]')
-    img_element = div_element.find_element(By.XPATH, './/img')
-    src_value = img_element.get_attribute('src')
-    return src_value
+    phone_element = driver.find_element(By.XPATH, '/html//span[@class="mhide"]').text
+    return phone_element
 
 
+#TODO
 def scrap_car_number(driver, target_url):
 
     driver.get(target_url)
-    div_element = driver.find_element(By.XPATH, '/html//div[@class="photo-620x465 loaded"]')
-    img_element = div_element.find_element(By.XPATH, './/img')
-    src_value = img_element.get_attribute('src')
-    return src_value
+
+    # div_element = driver.find_element(By.XPATH, '/html//span[@class="state-num ua"]')
+    # res = div_element.text
+    return "Cannot scrap"
 
 
+#TODO
 def scrap_vin(driver, target_url):
 
     driver.get(target_url)
-    div_element = driver.find_element(By.XPATH, '/html//div[@class="photo-620x465 loaded"]')
-    img_element = div_element.find_element(By.XPATH, './/img')
-    src_value = img_element.get_attribute('src')
-    return src_value
+
+    try:
+        vin = driver.find_element(By.XPATH, '/html//span[@class="vin-code"]').text
+    except:
+        vin_element = driver.find_element(By.XPATH, '/html//span[@class="label-vin"]').text
+        vin = vin_element
+    return vin
 
 
 if __name__ == '__main__':
@@ -141,5 +147,17 @@ if __name__ == '__main__':
     options.add_argument('--headless=chrome')
 
     with webdriver.Chrome(service=service, options=options) as driver:
-        driver.get(URL)
-        print()
+        driver.get(URL_USED)
+        WebDriverWait(driver, 3)
+        res = driver.find_elements(By.XPATH, '/html//a[@class ="m-link-ticket"]')
+        # for link in res:
+        #     print(link.get_attribute("href"))
+        
+
+
+
+
+
+
+
+
